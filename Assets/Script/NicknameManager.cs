c887f9a2a51d6c8797c314ce42dcf0541f4800c0
lipnus <sunpil13@korea.ac.kr>
2019-01-28 22:35:43 +0900

    add nickname scene.

---
+++ Assets/Script/NicknameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NicknameManager : MonoBehaviour {
+
+    public InputField inputField;
+    public Text sleepingText;
+    
+    
+    // Start is called before the first frame update
+    void Start() {
+        inputField.interactable = true;
+    }
+
+    public void nicknameChanged() {
+        if (inputField.text != "") {
+            sleepingText.text = "이곳은 " + inputField.text + "의 꿈속";
+        }
+        else {
+            sleepingText.text = "이곳은 꿈속";
+        }
+    }
+}
